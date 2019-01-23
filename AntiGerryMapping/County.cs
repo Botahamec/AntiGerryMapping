044a4ef95c925f86b2e54a1da31de153501999fa
Mike White <botahamec@outlook.com>
2019-01-23 15:38:48 -0500

    County class
    Added getters and setters to the County class

--- AntiGerryMapping/County.cs
+++ AntiGerryMapping/County.cs
@@ -8,8 +8,46 @@ namespace AntiGerryMapping
 {
 	class county {
 
+		//defining the properties
+		string name;
+		county[] borders;
+		int population;
+		Dictionary<string, int> demo;
+
 		//constructor
-		county(string name, county[] borders, int population, int votingPop, Dictionary<string, int> demo) { }
+		county(string name, county[] borders, int population, int votingpop, Dictionary<string, int> demo) {
+
+			this.name = name;
+			this.borders = borders;
+			this.population = votingpop;
+			
+			//finds percentages based on population and demo
+			foreach (KeyValuePair<string, int> dem in demo) {
+				this.demo.Add(dem.Key, dem.Value * votingpop / population);
+			}
+
+		}
+
+		//properties
+		public string Name {
+			get {return name;}
+			set {name = value;}
+		}
+
+		public county[] Borders {
+			get {return borders;}
+			set {borders = value;}
+		}
+
+		public int Pop {
+			get {return population;}
+			set {population = value;}
+		}
+
+		public Dictionary<string, int> Demo {
+			get {return demo;}
+			set {demo = value;}
+		}
 
 	}
 }
